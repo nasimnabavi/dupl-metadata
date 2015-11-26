11572e32bcab0328fd167a93b3a432bbddf10b88
nasimnabavi <nasim.nabavi@scrutinizer-ci.com>
2015-11-26 14:02:36 +0100

    Update Foo.php

--- src/Foo.php
+++ src/Foo.php
@@ -8,6 +8,8 @@ class Foo
         file_get_contents($_GET['something']);
     }
     public function fooDupl() {
+        
+        
         echo 'Foo';
         
         file_get_contents($_GET['something']);
