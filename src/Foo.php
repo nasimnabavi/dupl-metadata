7bf1c90682e92f675018d04c33a3d2949b397909
Nasim <nasim.nabavi@scrutinizer-ci.com>
2018-07-17 12:12:00 +0200

    phpcs

--- src/Foo.php
+++ src/Foo.php
@@ -3,28 +3,55 @@
 class Foo
 {
     public function foo(){echo 'Foo';
-        
+
         file_get_contents($_GET['something']);
     }
     public function fooDupl() {
 
         $b = true;
         if ($b) {echo "hi";}
-        
+
         echo 'Foo';
-    
-    
-    
-        
+
+
+
+
         file_get_contents($_GET['something']);
     }
     public function baz() {
-        
-        
-        
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
+        file_get_contents($_GET['something']);
+        echo 'Adds another function';
     }
-    public function addsAnotherMethod() { }
 
+
+    public function bazAdded() {}
     public function bar()
     {
         echo 'Adds another function';
---
+++ src/FooAdded.php
@@ -0,0 +1,30 @@
+<?php
+
+class FooAdded
+{
+    public function foo(){echo 'Foo';
+
+        file_get_contents($_GET['something']);
+    }
+    public function fooDupl() {
+
+        $b = true;
+        if ($b) {echo "hi";}
+
+        echo 'Foo';
+
+
+
+
+        file_get_contents($_GET['something']);
+    }
+    public function baz() {
+
+    }
+
+    public function bazAdded() {}
+    public function bar()
+    {
+        return true; // and another coding style issue
+    }
+}
